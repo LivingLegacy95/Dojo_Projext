ce29982fd6a07a530d0f314c33e1695095f98cc4
LivingLegacy95 <kgreene012@gmail.com>
2023-03-02 18:16:19 -0800

    4th commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var ["https://www.youtube.com/watch?v=JvmaMEfhQuE&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=1",
+    "https://www.youtube.com/watch?v=pb-6C9D05AM&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=2&t=293s",
+    "https://www.youtube.com/watch?v=L9Zm_8bJOCI&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=3&t=11s",
+    "https://www.youtube.com/watch?v=f0YWfZZyrYo&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=4&t=26s",
+    "https://www.youtube.com/watch?v=bDx94a9bPnw&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=5&t=5s",
+    "https://www.youtube.com/watch?v=BoILyA5xAFI&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=6&t=1s",
+    "https://www.youtube.com/watch?v=1NcK13W606M&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=7",
+    "https://www.youtube.com/watch?v=xs3w1B6Ys6k&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=8&t=10s",
+    "https://www.youtube.com/watch?v=86Wn91d6ZT4&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=9&t=9s",
+    "https://www.youtube.com/watch?v=EFswQ6uy59A&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=10&t=30s"]
