9f8eb04facf06e1b0a29905c155bff8ca3dfb4e8
LivingLegacy95 <kgreene012@gmail.com>
2023-04-21 16:01:34 -0700

    2nd commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var videos = ["https://www.youtube.com/watch?v=JvmaMEfhQuE&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=1",
-    "https://www.youtube.com/watch?v=pb-6C9D05AM&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=2&t=293s",
-    "https://www.youtube.com/watch?v=L9Zm_8bJOCI&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=3&t=11s",
-    "https://www.youtube.com/watch?v=f0YWfZZyrYo&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=4&t=26s",
-    "https://www.youtube.com/watch?v=bDx94a9bPnw&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=5&t=5s",
-    "https://www.youtube.com/watch?v=BoILyA5xAFI&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=6&t=1s",
+    "https://www.youtube.com/embed/pb-6C9D05AM",
+    "https://www.youtube.com/embed/EFswQ6uy59A",
+    "https://www.youtube.com/embed/f0YWfZZyrYo",
     "https://www.youtube.com/watch?v=1NcK13W606M&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=7",
-    "https://www.youtube.com/watch?v=xs3w1B6Ys6k&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=8&t=10s",
+    "https://www.youtube.com/embed/f0YWfZZyrYo",
     "https://www.youtube.com/watch?v=86Wn91d6ZT4&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=9&t=9s",
     "https://www.youtube.com/watch?v=EFswQ6uy59A&list=PLnp0SSa5LpQeTzBw1iFxxI4b-N2805bFf&index=10&t=30s"];
 
@@ -21,14 +19,25 @@ window.addEventListener("onload", function () {
 
 leftBtn.addEventListener("click", () => {
     for (let i = videos.length - 1; i > 1; i--) {
-        leftVideo.src = videos[i];
+        rightVideo.src = videos[i];
         videos.pop();
     }
 });
 
 rightBtn.addEventListener("click", () => {
     for (let i = videos.length - 1; i > 1; i--) {
-        rightVideo.src = videos[i];
+        leftVideo.src = videos[i];
         videos.pop();
     }
 });
+
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    angle: Math.PI / 8,
+    speedIn: 1,
+    speedOut: 0.8,
+    image1: '',
+    image2: './Images/Hokages/Hashirama_Senju_Main.webp',
+    displacementImage: 'Images\Hokages\Hashirama_Senju_Alt(1).jpg',
+})
